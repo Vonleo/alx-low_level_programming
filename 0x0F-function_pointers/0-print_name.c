191a6d606f486f80f46cdb6ae366599fd37df45c
Yvonne <yvonnegachara.yw@gmail.com>
2022-04-14 10:21:02 +0300

    Whats my name corrections

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,13 +3,13 @@
 
 /**
  * print_name - a function that prints a name.
- * name: pointer to name to print
- * f: function to print name
+ * @name: pointer to name to print
+ * @f: pointer to printing function
  * Return: Nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
+	if (!name || !f)
 		return;
 	f(name);
 }
